c85f1cb6955a2d1fab7466e46e7261a7dfa033db
daniellujanvillarreal <dlujan@niva.mx>
2015-10-14 09:30:41 -0500

    fixed review comments

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/adapters/ScoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/adapters/ScoresAdapter.java
@@ -1,4 +1,4 @@
-package barqsoft.footballscores;
+package barqsoft.footballscores.adapters;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,10 +12,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.ViewHolder;
+import barqsoft.footballscores.utils.Utilies;
+
 /**
  * Created by yehya khaled on 2/26/2015.
  */
-public class scoresAdapter extends CursorAdapter
+public class ScoresAdapter extends CursorAdapter
 {
     public static final int COL_HOME = 3;
     public static final int COL_AWAY = 4;
@@ -32,7 +36,7 @@ public class scoresAdapter extends CursorAdapter
     public double detail_match_id = 0;
 
     private String FOOTBALL_SCORES_HASHTAG = "#Football_Scores";
-    public scoresAdapter(Context context,Cursor cursor,int flags)
+    public ScoresAdapter(Context context, Cursor cursor, int flags)
     {
         super(context,cursor,flags);
     }
@@ -56,7 +60,7 @@ public class scoresAdapter extends CursorAdapter
         mHolder.home_name.setText(cursor.getString(COL_HOME));
         mHolder.away_name.setText(cursor.getString(COL_AWAY));
         mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
+        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS)));
         mHolder.match_id = cursor.getDouble(COL_ID);
 
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
